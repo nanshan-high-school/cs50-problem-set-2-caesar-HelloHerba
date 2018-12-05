bc45330398ea78954435fa114dc2514d1d02d9a8
25138王科貿 <43640555+HelloHerba@users.noreply.github.com>
2018-12-06 00:10:43 +0800

    Update Caesar cipher  by25138王科貿.cpp

--- Caesar cipher  by25138王科貿.cpp
+++ Caesar cipher  by25138王科貿.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 using namespace std;
+const int number_add = 5 ;
 int main() {
   int num ;
   string password ;
   cout << "請輸入一個密碼:" ;
   cin >> password;
   int i=password.length() ;//表示i等於陣列總使用位數
-  cout << "\n請輸入要遞移的數字:" ;
-  cin >> num;
-  const int number_add = num ;
   cout<<"\n加密後的結果為:" ;
   for (int j=0 ; j<i ; j++)
   {
@@ -16,11 +14,13 @@ int main() {
     {
       cout << password[j] ;
       continue ;
-    }
-    //如果輸入的內容為符號,則不做任何改動
+    }//如果輸入的內容為符號,則不做任何改動
     password[j] = password[j] + number_add ;
-    //將非符號的字元遞移
-    if (password[j] > 'Z' || password[j] < 'a' && password[j] > 'z' )
+     //將非符號的字元遞移
+    if(password[j]-number_add >='a'&& password[j]+number_add>'z')
+    {
+        password[j]=password[j]-26;
+    }else if (password[j]-number_add <'Z' && password[j] > 'Z' )
     {
       password[j] = password[j]-26 ;
     }
